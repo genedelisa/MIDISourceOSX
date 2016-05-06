72edc2e94a59e71a5d53991ac74e38139a84cbc3
Gene De Lisa <gene@rockhoppertech.com>
2016-05-05 21:43:46 -0400

    Initial Commit

---
+++ MIDISourceOSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MIDISourceOSX
+//
+//  Created by Gene De Lisa on 5/5/16.
+//  Copyright © 2016 Gene De Lisa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MIDISourceOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MIDISourceOSX
+//
+//  Created by Gene De Lisa on 5/5/16.
+//  Copyright © 2016 Gene De Lisa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
